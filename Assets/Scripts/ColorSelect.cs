29d6aa066d44149ba4c2f1ecd64cf142e140df05
rudrjain <rudr.jainr@gmail.com>
2020-01-09 01:23:17 +0530

    AR Watches

---
+++ Assets/Scripts/ColorSelect.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColorSelect : MonoBehaviour
+{
+    public Material band1, band2, band3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void w1color()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+        if(buttonName=="BlackButton")
+        {
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#000000", out myColor);
+            band1.color = myColor;
+        }
+
+        else if (buttonName == "BlueButton")
+        {
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#1243A2", out myColor);
+            band1.color = myColor;
+        }
+
+    }
+
+    public void w2color()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+        if (buttonName == "BlackButton")
+        {
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#000000", out myColor);
+            band2.color = myColor;
+        }
+
+        else if (buttonName == "RedButton")
+        {
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#740202", out myColor);
+            band2.color = myColor;
+        }
+    }
+
+    public void w3color()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+        if (buttonName == "BlackButton")
+        {
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#919191", out myColor);
+            band3.color = myColor;
+        }
+
+        else if (buttonName == "SilverButton")
+        {
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#D8DCE7", out myColor);
+            band3.color = myColor;
+        }
+    }
+}
---
+++ Assets/Scripts/WatchSelect.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WatchSelect : MonoBehaviour
+{
+    public GameObject watch1, watch2, watch3;
+    public GameObject w1, w2, w3;
+    Animation window1, window2, window3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        window1 = w1.GetComponent<Animation>();
+        window2 = w2.GetComponent<Animation>();
+        window3 = w3.GetComponent<Animation>();
+
+    }
+
+    public void w1click()
+    {
+        watch1.SetActive(true);
+        watch2.SetActive(false);
+        watch3.SetActive(false);
+
+        window1["W1"].speed = 1;
+        window1.Play();
+    }
+    public void w2click()
+    {
+        watch1.SetActive(false);
+        watch2.SetActive(true);
+        watch3.SetActive(false);
+
+        window2["W2"].speed = 1;
+        window2.Play();
+    }
+    public void w3click()
+    {
+        watch1.SetActive(false);
+        watch2.SetActive(false);
+        watch3.SetActive(true);
+
+        window3["W3"].speed = 1;
+        window3.Play();
+    }
+
+    public void close()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+
+        if(buttonName=="w1close")
+        {
+            window1["W1"].speed = -1;
+            window1["W1"].time = window1["W1"].length;
+            window1.Play();
+        }
+
+        if (buttonName == "w2close")
+        {
+            window2["W2"].speed = -1;
+            window2["W2"].time = window2["W2"].length;
+            window2.Play();
+        }
+
+        if (buttonName == "w3close")
+        {
+            window3["W3"].speed = -1;
+            window3["W3"].time = window3["W3"].length;
+            window3.Play();
+        }
+    }
+}
